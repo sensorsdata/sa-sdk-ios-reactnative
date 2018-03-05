b4ed2de8b1598c8a8c7c2e7c1ce5c8eec9b9e7b4
xiaoyanmin <xiaoyanmin@sensorsdata.cn>
2018-03-05 19:23:49 +0800

    Release 1.0.0

---
+++ RNSensorsAnalyticsModule.h
@@ -0,0 +1,14 @@
+//
+//  RNSensorsAnalyticsModule.h
+//  hunheDemo
+//
+//  Created by 肖彦敏 on 2017/4/14.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RNSensorsAnalyticsModule : NSObject<RCTBridgeModule>
+
+@end
